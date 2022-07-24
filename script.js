d4458a1901a4b67f7e0785ca901e43ad79ed9e6d
jun6000 <arjunvishanth@gmail.com>
2022-07-24 13:07:45 +0530

    Fixed callback hell and modularized code into functions; also added keyboard input support for parentheses operators.

--- script.js
+++ script.js
@@ -1,107 +1,91 @@
 /*
 TODO
 enable hover effect when using keyboard input
-cleanup code
 */
 
-// Main
-const box = document.getElementsByClassName("shell")[0];
-const display = document.getElementById("displayText");
-let exp = ["", ""];
+window.onload = init;
 
-// Input by clicking buttons
-box.addEventListener("click", e => {
-    if(display.textContent.length >= 36) display.textContent = "...";
-    if(e.target) {
-        if(exp[0] === "Error!") {
-            exp = ["", ""];
-            display.textContent = "";
-        }
-        if(e.target.matches("div.number")) {
-            if(exp[1] === "ans" && exp[0] != "") {
-                exp[0] = e.target.innerHTML;
-                display.textContent = exp[0];
-                exp[1] = "";
-            }
-            else {
-                display.textContent += e.target.innerHTML;
-                exp[0] += e.target.innerHTML;
-            }
-        }
-        else if(e.target.matches("div.operator")) {
-            display.textContent += e.target.innerHTML;
-            exp[1] = "";
-            if(e.target.id === "multiply") exp[0] += "*";
-            else if(e.target.id === "divide") exp[0] += "/";
-            else exp[0] += e.target.innerHTML;
-        }
-        else if(e.target.matches("div.allClear")) {
-            display.textContent = "";
-            exp = ["", ""];
-        }
-        else if(e.target.matches("div.equals")) {
-            if(exp[0] !== "") {
-                try {
-                    display.textContent = math.evaluate(exp[0]);
-                }
-                catch(e) {
-                    display.textContent = "Error!";
-                }
-                exp[0] = display.textContent;
-                exp[1] = "ans";
-            }
-        }
-    }
-});
+// Function definitions
+function init() {
+    const box = document.getElementsByClassName("shell")[0];
+    const display = document.getElementById("displayText");
+    let exp = ["", ""];
 
-// Input from keyboard
-document.addEventListener("keydown", e => {
-    if(exp[0] === "Error!") {
-        exp = ["", ""];
-        display.textContent = "";
+    box.addEventListener("click", e => { handleButtonClick(e, display, exp); }, false);
+    document.addEventListener("keydown", e => { handleKeyDown(e, display, exp); }, false);
+}
+
+function handleButtonClick(e, display, exp) { // Input by clicking buttons
+    if (e.target) {
+        if (display.textContent.length >= 36) display.textContent = "..."; // Display overflow
+        if (exp[0] === "Error!") clear(display, exp);
+        if (e.target.matches("div.number")) inputNumber(e, display, exp, "click");
+        else if (e.target.matches("div.operator")) inputOperator(e, display, exp, "click");
+        else if (e.target.matches("div.allClear")) clear(display, exp);
+        else if (e.target.matches("div.equals")) solve(display, exp);
     }
-    if(e.key === "Backspace") {
+}
+
+function handleKeyDown(e, display, exp) { // Input from keyboard
+    if (exp[0] === "Error!") clear(display, exp);
+    if (e.key === "Backspace") {
         display.textContent = display.textContent.slice(0, display.textContent.length - 1);
         exp[0] = display.textContent;
     }
-    else if(e.key === "Enter") {
-        if(exp[0] !== "") {
-            try {
-                display.textContent = math.evaluate(exp[0]);
-            }
-            catch(e) {
-                display.textContent = "Error!";
-            }
-            exp[0] = display.textContent;
-            exp[1] = "ans";
-        }
-    }
-    else if(/^\d+$/.test(e.key) || e.key === ".") {
-        if(exp[1] === "ans" && exp[0] != "") {
-            exp[0] = e.key;
-            display.textContent = exp[0];
-            exp[1] = "";
-        }
-        else {
-            display.textContent += e.key;
-            exp[0] += e.key;
-        }
+    else if (e.key === "Enter") solve(display, exp);
+    else if (/^\d+$/.test(e.key) || e.key === ".") inputNumber(e, display, exp, "key");
+    else if (/^[+\-*%/()]$/.test(e.key)) inputOperator(e, display, exp, "key");
+}
+
+function clear(display, exp) {
+    exp[0] = "";
+    exp[1] = "";
+    display.textContent = "";
+}
+
+function inputNumber(e, display, exp, inputType) {
+    let target = (inputType === "click") ? e.target.innerHTML : e.key;
+    if (exp[1] === "ans" && exp[0] != "") {
+        exp[0] = target;
+        display.textContent = exp[0];
+        exp[1] = "";
     }
     else {
-        exp[1] = "";
-        if(e.key === "+" || e.key === "-" || e.key === "%") {
-            display.textContent += e.key;
-            exp[0] += e.key;
-        }
-        else if(e.key === "/") {
-            const div = document.getElementById("divide");
-            display.textContent += div.innerHTML;
-            exp[0] += e.key;
-        }
-        else if(e.key === "*") {
-            const div = document.getElementById("multiply");
-            display.textContent += div.innerHTML;
-            exp[0] += e.key;
-        }
+        display.textContent += target;
+        exp[0] += target;
     }
-});
+}
+
+function inputOperator(e, display, exp, inputType) {
+    exp[1] = "";
+    if (inputType === "click") {
+        display.textContent += e.target.innerHTML;
+        if (e.target.id === "multiply") exp[0] += "*";
+        else if (e.target.id === "divide") exp[0] += "/";
+        else exp[0] += e.target.innerHTML;
+    }
+    else {
+        if (e.key === "+" || e.key === "-"
+            || e.key === "%" || e.key === "("
+            || e.key === ")") display.textContent += e.key;
+        else if (e.key === "*" || e.key === "/") {
+            const opID = (e.key === "*") ? "multiply" : "divide";
+            const div = document.getElementById(opID);
+            display.textContent += div.innerHTML;
+        }
+        exp[0] += e.key;
+    }
+}
+
+function solve(display, exp) {
+    if (exp[0] !== "") {
+        try {
+            display.textContent = math.evaluate(exp[0]);
+        }
+        catch(e) {
+            display.textContent = "Error!";
+        }
+        exp[0] = display.textContent;
+        exp[1] = "ans";
+    }
+}
